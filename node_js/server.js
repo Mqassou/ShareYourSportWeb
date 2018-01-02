ddf3e8db2653f1640618769b01b5fe651fe27fdf
qas <qassoumohamed@gmail.com>
2018-01-02 16:17:27 +0100

    ajout d'express, body parser, mongodb + fonctionnalite login

---
+++ node_js/server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const url = require('url');
+const MongoClient = require('mongodb').MongoClient;
+var bodyParser =  require("body-parser");
+const querystring = require('querystring');
+const express=require('express');
+const app =express();
+
+const urlMongo = "mongodb://localhost:27017";
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/login',function(req,res){
+
+    const email = req.body.email;
+    const motdepasse = req.body.motdepasse
+
+    MongoClient.connect(urlMongo, function (err, client) {
+    if (err) throw err;
+
+    const db = client.db('shareyoursport');
+    const query = {"motdepasse":motdepasse,"email":email};
+
+    db.collection('utilisateurs').findOne(query,function (findErr, result) {
+    if (findErr) throw findErr;
+    if(result!=null)
+    {
+        if(email===result.email && motdepasse===result.motdepasse)
+        {
+        res.json({"login":"true"});
+        }
+     }
+   else
+    {
+            res.json({"login":"false"});
+    } 
+    client.close();
+    });
+
+}); 
+
+});//End of /login
+
+app.listen(8080);
