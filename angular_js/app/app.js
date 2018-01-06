dd1d510b9c38861849a24c6b392e476f3c1c250e
qas <qassoumohamed@gmail.com>
2018-01-06 16:28:57 +0100

    t

--- angular_js/app/app.js
+++ angular_js/app/app.js
@@ -2,15 +2,21 @@
 
 // Declare app level module which depends on views, and components
 <<<<<<< HEAD
+<<<<<<< HEAD
 angular.module('ShareYourSport', [
   'ShareYourSport.ui-map',
   'ngRoute'
 =======
+=======
+>>>>>>> parent of c34dbcf...  ajout service login, ajout boostrap template, creation page home
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
+<<<<<<< HEAD
+>>>>>>> parent of c34dbcf...  ajout service login, ajout boostrap template, creation page home
+=======
 >>>>>>> parent of c34dbcf...  ajout service login, ajout boostrap template, creation page home
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
