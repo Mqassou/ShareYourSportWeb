de921ecd19cd55f90a1f9fcc3742f07682880e91
qas <qassoumohamed@gmail.com>
2018-01-06 16:11:11 +0100

    truc

--- angular_js/app/app.js
+++ angular_js/app/app.js
@@ -2,6 +2,7 @@
 
 // Declare app level module which depends on views, and components
 angular.module('ShareYourSport', [
+  'ShareYourSport.ui-map',
   'ngRoute'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
---
+++ angular_js/app/map/map.module.js
@@ -0,0 +1,1 @@
+var myAppModule = angular.module('ShareYourSport.ui-map', ['ui.map']);
