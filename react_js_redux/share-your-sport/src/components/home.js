f8400aaef2b02c7f2e990ea8a77f7489e0fc1884
qas <qassoumohamed@gmail.com>
2018-01-19 14:56:01 +0100

    installation react router

---
+++ react_js_redux/share-your-sport/src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class Home extends React.Component {
+  scream() {
+    alert('AAAAAAAAHHH!!!!!');
+  }
+
+  render() {
+    return <h1>Home</h1>;
+  }
+}
+
---
+++ react_js_redux/share-your-sport/src/components/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class Login extends React.Component {
+
+
+  render() {
+    return <h1>Login</h1>;
+  }
+}
+
